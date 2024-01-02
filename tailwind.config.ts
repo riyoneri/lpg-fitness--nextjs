0a0d051f20abd82559771ddaee25deeaae0baac0
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-02 20:43:50 +0200

    feat: create trapezium clipPath

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindClipPath from "tailwind-clip-path";
 
 const config: Config = {
   content: [
@@ -6,8 +7,12 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      clipPath: {
+        trapezium: "polygon(20% 0%, 100% 0%, 100% 100%, 0% 0%)",
+      },
+    },
   },
-  plugins: [],
+  plugins: [tailwindClipPath],
 };
 export default config;
