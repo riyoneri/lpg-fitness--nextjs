7810f67453b187f7bcc40b8d9597b82ded1f3126
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-03 20:36:16 +0200

    feat: mobile navigation

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -8,17 +8,26 @@ import { HamburgerCollapse } from "react-animated-burgers";
 import { useEffect, useState } from "react";
 
 export default function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isMount, setIsMount] = useState(false);
   useEffect(() => setIsMount(true), []);
+
+  const handleNavOpenChange = () => {
+    if (isNavOpen) {
+      setIsMenuOpen(false);
+      setTimeout(() => setIsNavOpen(false), 500);
+    } else {
+      setIsNavOpen(true);
+      setIsMenuOpen(true);
+    }
+  };
+
   return (
     <div
-      className={classNames(
-        "width-responsive pt-10 relative grid",
-        questrial.className,
-      )}
+      className={classNames("width-responsive pt-10 grid", questrial.className)}
     >
-      <nav className="hidden items-center justify-between">
+      <nav className="hidden relative lg:flex items-center justify-between">
         <Link
           href="/"
           className={classNames(montserrat.className, "font-bold text-xl")}
@@ -52,6 +61,77 @@ export default function NavBar() {
           </span>
         </Link>
       </nav>
+      <div className="grid lg:hidden place-content-center justify-self-end">
+        {isMount && (
+          <HamburgerCollapse
+            toggleButton={handleNavOpenChange}
+            isActive={isMenuOpen}
+            buttonStyle={{
+              padding: 0,
+              zIndex: 10,
+              position: "relative",
+              top: 0,
+              transitionDuration: "100ms",
+              transitionDelay: "2s",
+            }}
+            buttonWidth={30}
+            barColor={isNavOpen ? "rgb(255 255 255)" : "rgb(30 30 30)"}
+          />
+        )}
+
+        <nav
+          className={classNames(
+            "text-white absolute inset-0 px-5 bg-black/50 flex flex-col items-center pt-10 gap-10",
+            {
+              ["block"]: isNavOpen,
+              ["hidden"]: !isNavOpen,
+            },
+          )}
+        >
+          <Link
+            onClick={handleNavOpenChange}
+            href="/"
+            className={classNames(montserrat.className, "font-bold text-xl")}
+          >
+            <span className="text-custom-orange">LPG</span>
+            <span>Fitness</span>
+          </Link>
+          <div
+            className={
+              "grid *:text-center w-full *:py-1 sm:*:py-2 *:rounded-full sm:w-1/2 gap-5"
+            }
+          >
+            <Link
+              onClick={handleNavOpenChange}
+              href="/"
+              className="hover:bg-custom-orange"
+            >
+              About
+            </Link>
+            <Link
+              onClick={handleNavOpenChange}
+              href="/"
+              className="hover:bg-custom-orange"
+            >
+              Trainers
+            </Link>
+            <Link
+              onClick={handleNavOpenChange}
+              href="/"
+              className="hover:bg-custom-orange"
+            >
+              Pricing
+            </Link>
+            <Link
+              onClick={handleNavOpenChange}
+              href="/"
+              className="hover:bg-custom-orange"
+            >
+              Sucess Story
+            </Link>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
