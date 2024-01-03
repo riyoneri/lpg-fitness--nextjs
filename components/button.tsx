6a7df355728f760d82e569388838972ef69958ee
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-03 21:37:40 +0200

    feat: create button component

---
+++ components/button.tsx
@@ -0,0 +1,47 @@
+import classNames from "classnames";
+import Link from "next/link";
+import { BiRightArrowAlt } from "react-icons/bi";
+
+interface ButtonProperties {
+  link: string;
+  text: string;
+  dark?: boolean;
+  className?: string;
+}
+
+export default function Button({
+  link,
+  text,
+  dark,
+  className,
+}: ButtonProperties) {
+  return (
+    <Link
+      href={link}
+      className={classNames(
+        "flex gap-2 items-center group rounded-full p-1 transition border-2",
+        {
+          "bg-light-black hover:bg-custom-orange border-white": dark,
+          "bg-custom-orange hover:bg-white border-light-black": !dark,
+        },
+        className,
+      )}
+    >
+      <span
+        className={classNames("px-4 text-white transition", {
+          "group-hover:text-neutral-950": !dark,
+        })}
+      >
+        {text}
+      </span>
+      <span
+        className={classNames("p-1 rounded-full", {
+          "bg-custom-orange": dark,
+          "bg-light-black": !dark,
+        })}
+      >
+        <BiRightArrowAlt className="size-5 group-hover:translate-x-1.5 transition duration-300" />
+      </span>
+    </Link>
+  );
+}
