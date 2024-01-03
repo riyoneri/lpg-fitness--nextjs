38d8315991857cd5603d424850f64aeb8c505f77
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-03 20:57:55 +0200

    refactor: add width-responsive class to page

--- app/sections/hero.tsx
+++ app/sections/hero.tsx
@@ -3,10 +3,10 @@ import NavBar from "@/components/nav-bar";
 
 export default function Hero() {
   return (
-    <section className="min-h-dvh relative">
+    <section className="min-h-dvh py-10 width-responsive pt-10">
       <BackgroundShapes displayImage />
       <NavBar />
-      <div className="relative width-responsive"></div>
+      <div className="relative bg-red-500 h-full">a</div>
     </section>
   );
 }
