ae534f8953a68e055ba91c2350ea6eb895c5f93c
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-04 17:06:52 +0200

    refactor: change bg-light-black -> bg-black-light in background shapes

--- components/background-shapes.tsx
+++ components/background-shapes.tsx
@@ -15,7 +15,7 @@ export default function BackgroundShapes({
     <div className="absolute bg-custom-orange shadow-inner shadow-black/90 inset-0">
       <div
         className={classNames(
-          "w-1/2 relative sm:w-auto bg-light-black h-full",
+          "w-1/2 relative sm:w-auto bg-black-light h-full",
           {
             "sm:clip-path-bg-shape": !flipShapes,
             "sm:clip-path-bg-shape-fliped left-1/2 sm:left-0": flipShapes,
@@ -29,7 +29,7 @@ export default function BackgroundShapes({
           className="absolute w-1/2 hidden xs:block h-full object-cover sm:object-contain right-0 bottom-0"
         />
       )}
-      <div className="absolute h-full w-full bottom-0 bg-gradient-to-b from-light-black/20 to-light-black"></div>
+      <div className="absolute h-full w-full bottom-0 bg-gradient-to-b from-black-light/20 to-black-light"></div>
     </div>
   );
 }
