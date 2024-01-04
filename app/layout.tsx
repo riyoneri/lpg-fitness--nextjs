b1f7cca2238c8e10c3c1d71631cc937e58ceff65
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-04 17:06:28 +0200

    refactor: change bg-light-black -> bg-black-light in rootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={classNames(questrial.className, "text-white bg-light-black")}
+        className={classNames(questrial.className, "text-white bg-black-light")}
       >
         {children}
       </body>
