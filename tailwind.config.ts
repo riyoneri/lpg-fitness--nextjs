4aeaab1e7ebe90c47e10cfba1732ad97f10ad015
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-04 17:07:43 +0200

    feat: add shapes in mobile screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,12 +12,15 @@ const config: Config = {
         "bg-shape": "polygon(0% 0%, 60% 0%, 45% 75%, 100% 100%, 0% 100%)",
         "bg-shape-fliped":
           "polygon(40% 0%, 100% 0%, 100% 100%, 0% 100%, 55% 75%)",
-        "center-info": "polygon(0% 0%, 100% 0%, 97% 100%, 3% 100%)",
         "left-info": "polygon(0% 20%, 100% 20%, 100% 100%, 0% 85%)",
         "right-info": "polygon(0% 20%, 100% 20%, 100% 85%, 0% 100%)",
+        "center-info": "polygon(0% 0%, 100% 0%, 97% 100%, 3% 100%)",
+        "middle-info-sm": "polygon(4% 0%, 96% 0%, 94% 100%, 6% 100%)",
+        "bottom-info-sm": "polygon(7% 0%, 93% 0%, 90% 100%, 9% 100%)",
       },
       colors: {
-        "light-black": "#1E1E1E",
+        "black-light": "#1E1E1E",
+        "black-lighter": "#3D3D3D",
         "custom-orange": "#FF3D00",
       },
       boxShadow: {
