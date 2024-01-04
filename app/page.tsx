3c264bd9acc2f2031b8d8a680dbe13c12772944b
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-04 19:37:58 +0200

    refactor: apply width-responsive to whole page container

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import About from "./sections/about";
 import Hero from "./sections/hero";
 import Info from "./sections/info";
 
 export default function Home() {
   return (
-    <main className="*:my-10 *:width-responsive">
+    <main className="*:my-10 width-responsive">
       <Hero />
       <Info />
     </main>
