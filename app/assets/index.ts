40cb64f7e32d518111126faf8e156e8ff4e40b50
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-05 14:24:47 +0200

    refactor: export trainer images as assets

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -21,5 +21,13 @@ export { default as privateTraining } from "./images/private-training.png";
 export { default as buildMuscle } from "./images/build-muscle.png";
 export { default as about } from "./images/about.png";
 
+export { default as trainer1 } from "./images/trainer-1.png";
+export { default as trainer2 } from "./images/trainer-2.png";
+export { default as trainer3 } from "./images/trainer-3.jpg";
+export { default as trainer4 } from "./images/trainer-4.jpg";
+export { default as trainer5 } from "./images/trainer-5.jpg";
+export { default as trainer6 } from "./images/trainer-6.jpg";
+export { default as trainer7 } from "./images/trainer-7.jpg";
+
 // SVG
 export { default as InfoShape } from "./svg/info-shape";
