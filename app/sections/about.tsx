cb3b55743ffe1a7d9e7a0ad317ac64f8095e5c95
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-05 17:14:16 +0200

    feat: first steps of vidstack configuration in about us

--- app/sections/about.tsx
+++ app/sections/about.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Button from "@/components/button";
 import classNames from "classnames";
 import { montserrat } from "../assets";
+import "@vidstack/react/player/styles/base.css";
 
 export default function About() {
   return (
@@ -21,7 +24,22 @@ export default function About() {
         </p>
         <Button text="Get started" link="/" className="justify-self-start" />
       </div>
-      <div>videos</div>
+      <div className="grid gap-5 sm:grid-cols-3">
+        <div className="h-10 bg-custom-orange"></div>
+        <div className="h-10 bg-custom-orange"></div>
+        <div className="h-10 bg-custom-orange"></div>
+        {/* <MediaPlayer
+          src="/easy.mp4"
+          title="Workout 1"
+          className="h-20 overflow-hidden bg-green-500 relative"
+        >
+          <MediaProvider />
+          <PlayButton className="group p-1 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid place-content-center">
+            <PlayIcon className="size-10 hidden group-data-[paused]:block bg-green-500" />
+            <PauseIcon className="size-10 group-data-[paused]:hidden bg-green-500" />
+          </PlayButton>
+        </MediaPlayer> */}
+      </div>
     </section>
   );
 }
