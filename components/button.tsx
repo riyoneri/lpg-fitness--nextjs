64969ad05202f5405eee3e96a6444fb29ef60cbb
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-05 17:03:54 +0200

    refactor: alternate button with links

--- components/button.tsx
+++ components/button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { BiRightArrowAlt } from "react-icons/bi";
 
 interface ButtonProperties {
-  link: string;
+  link?: string;
   text: string;
   dark?: boolean;
   className?: string;
@@ -15,7 +15,7 @@ export default function Button({
   dark,
   className,
 }: ButtonProperties) {
-  return (
+  return link ? (
     <Link
       href={link}
       className={classNames(
@@ -43,5 +43,32 @@ export default function Button({
         <BiRightArrowAlt className="size-5 group-hover:translate-x-1.5 transition duration-300" />
       </span>
     </Link>
+  ) : (
+    <button
+      className={classNames(
+        "flex gap-2 items-center group rounded-full p-1 transition border-2",
+        {
+          "bg-black-light hover:bg-custom-orange border-white": dark,
+          "bg-custom-orange hover:bg-white border-black-light": !dark,
+        },
+        className,
+      )}
+    >
+      <span
+        className={classNames("px-4 text-white transition", {
+          "group-hover:text-neutral-950": !dark,
+        })}
+      >
+        {text}
+      </span>
+      <span
+        className={classNames("p-1 rounded-full", {
+          "bg-custom-orange": dark,
+          "bg-black-light": !dark,
+        })}
+      >
+        <BiRightArrowAlt className="size-5 group-hover:translate-x-1.5 transition duration-300" />
+      </span>
+    </button>
   );
 }
