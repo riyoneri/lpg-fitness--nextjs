f8cbbec8a19d6e54ecd3b5311dac634522488a69
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-07 19:20:09 +0200

    refactor: add appropriate height to stories text

--- components/story-list-item.tsx
+++ components/story-list-item.tsx
@@ -5,17 +5,26 @@ import Image from "next/image";
 
 export default function StoryListItem({ image, name, story }: Story) {
   return (
-    <div className="relative h-96">
+    <div className="relative">
       <Image
         src={image}
         alt={`${name} image`}
-        className="size-full object-top object-cover"
+        className="xs:size-full h-96 object-top xs:object-center object-cover"
+        width={500}
+        height={500}
       />
-      <div className="absolute inset-0 bg-black/40 flex flex-col *:px-5 gap-5 justify-end">
-        <h3 className={classNames(montserrat.className, "font-medium text-xl")}>
+      <div className="absolute inset-0 bg-black/40 bottom-0 flex flex-col gap-5 justify-end">
+        <h3
+          className={classNames(
+            montserrat.className,
+            "font-medium text-xl px-5",
+          )}
+        >
           {name}
         </h3>
-        <p className="bg-black py-5">&quot; {story} &quot;</p>
+        <p className="bg-black px-2 xs:px-5 xs:py-5 text-sm xs:text-base h-1/4 md:h-auto lg:h-1/3 2xl:h-1/5">
+          &quot;{story}&quot;
+        </p>
       </div>
     </div>
   );
