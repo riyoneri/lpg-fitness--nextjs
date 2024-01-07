2c45a2d34e0b2907114982eb8836cf729cd04c89
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-07 08:23:12 +0200

    refactor: add padding y for navigation on about section

--- app/sections/about.tsx
+++ app/sections/about.tsx
@@ -7,38 +7,41 @@ import "@vidstack/react/player/styles/base.css";
 
 export default function About() {
   return (
-    <section className="rounded-3xl p-5 sm:p-10 bg-about-image bg-blend-multiply bg-cover bg-center space-y-10">
-      <h1
-        className={classNames(
-          montserrat.className,
-          "text-3xl lg:text-5xl font-bold",
-        )}
-      >
-        About us
-      </h1>
-      <div className="lg:w-1/2 grid gap-5 sm:gap-10">
-        <p className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, earum
-          reiciendis? Modi esse voluptas, vel tempora, quod qui voluptate animi
-          vero totam, quaerat labore cupiditate non corrupti amet architecto ea!
-        </p>
-        <Button text="Get started" link="/" className="justify-self-start" />
-      </div>
-      <div className="grid gap-5 sm:grid-cols-3">
-        <div className="h-10 bg-custom-orange"></div>
-        <div className="h-10 bg-custom-orange"></div>
-        <div className="h-10 bg-custom-orange"></div>
-        {/* <MediaPlayer
-          src="/easy.mp4"
-          title="Workout 1"
-          className="h-20 overflow-hidden bg-green-500 relative"
+    <section className="py-16" id="about">
+      <div className="rounded-3xl p-5 sm:p-10 bg-about-image bg-blend-multiply bg-cover bg-center space-y-10">
+        <h1
+          className={classNames(
+            montserrat.className,
+            "text-3xl lg:text-5xl font-bold",
+          )}
         >
-          <MediaProvider />
-          <PlayButton className="group p-1 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid place-content-center">
-            <PlayIcon className="size-10 hidden group-data-[paused]:block bg-green-500" />
-            <PauseIcon className="size-10 group-data-[paused]:hidden bg-green-500" />
-          </PlayButton>
-        </MediaPlayer> */}
+          About us
+        </h1>
+        <div className="lg:w-1/2 grid gap-5 sm:gap-10">
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, earum
+            reiciendis? Modi esse voluptas, vel tempora, quod qui voluptate
+            animi vero totam, quaerat labore cupiditate non corrupti amet
+            architecto ea!
+          </p>
+          <Button text="Get started" link="/" className="justify-self-start" />
+        </div>
+        <div className="grid gap-5 sm:grid-cols-3">
+          <div className="h-10 bg-custom-orange"></div>
+          <div className="h-10 bg-custom-orange"></div>
+          <div className="h-10 bg-custom-orange"></div>
+          {/* <MediaPlayer
+            src="/easy.mp4"
+            title="Workout 1"
+            className="h-20 overflow-hidden bg-green-500 relative"
+          >
+            <MediaProvider />
+            <PlayButton className="group p-1 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid place-content-center">
+              <PlayIcon className="size-10 hidden group-data-[paused]:block bg-green-500" />
+              <PauseIcon className="size-10 group-data-[paused]:hidden bg-green-500" />
+            </PlayButton>
+          </MediaPlayer> */}
+        </div>
       </div>
     </section>
   );
