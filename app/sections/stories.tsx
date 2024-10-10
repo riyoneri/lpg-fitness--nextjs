eb23fa7740235fdf488f370f2d0888c0c0602261
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-10 20:06:05 +0200

    refactor: update successfull stories images imports

--- app/sections/stories.tsx
+++ app/sections/stories.tsx
@@ -1,13 +1,14 @@
 import classNames from "classnames";
-import { montserrat } from "../assets";
+import { montserrat, story1, story2, story3, story4 } from "../assets";
 import StoryListItem from "@/components/story-list-item";
 import BackgroundShapes from "@/components/background-shapes";
+import { StaticImageData } from "next/image";
 
 export interface Story {
   id: number;
   name: string;
   story: string;
-  image: string;
+  image: StaticImageData;
 }
 
 const StoriesArray: Story[] = [
@@ -16,31 +17,27 @@ const StoriesArray: Story[] = [
     name: "Derek",
     story:
       "Derek improved his strength and endurance significantly at LPG Fitness",
-    image:
-      "https://images.unsplash.com/flagged/photo-1576579206905-df3aa87352b6?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Zml0bmVzcyUyMGJveXxlbnwwfHwwfHx8MA%3D%3D",
+    image: story1,
   },
   {
     id: 1,
     name: "Isabella",
     story: "Isabella reached her fitness milestones at LPG Fitness",
-    image:
-      "https://images.unsplash.com/photo-1606902965551-dce093cda6e7?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Zml0bmVzcyUyMGdpcmx8ZW58MHx8MHx8fDA%3D",
+    image: story2,
   },
   {
     id: 2,
     name: "John",
     story:
       "John gained 10 pounds of lean muscle through our specialized program",
-    image:
-      "https://images.unsplash.com/photo-1581009146145-b5ef050c2e1e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zml0bmVzcyUyMGJveXxlbnwwfHwwfHx8MA%3D%3D",
+    image: story3,
   },
   {
     id: 3,
     name: "Sophia",
     story:
       "Sophia improved her fitness and discovered a passion for wellness at LPG Fitness",
-    image:
-      "https://images.unsplash.com/photo-1470468969717-61d5d54fd036?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZpdG5lc3MlMjBnaXJsfGVufDB8fDB8fHww",
+    image: story4,
   },
 ];
 
@@ -59,7 +56,7 @@ export default function Stories() {
             "font-medium text-2xl text-center relative",
           )}
         >
-          Success Stries
+          Success Stories
         </h1>
         <p className="text-center relative">
           Be inspired by the transformational journeys of our members at LPG
